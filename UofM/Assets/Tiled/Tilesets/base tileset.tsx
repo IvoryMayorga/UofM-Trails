d5c94f6a5a7e8323b74749504da877267b34aab0
IvoryMayorga <ivorysmay@gmail.com>
2023-04-16 18:04:20 -0500

    Updates

--- UofM/Assets/Tiled/Tilesets/base tileset.tsx
+++ UofM/Assets/Tiled/Tilesets/base tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="base tileset" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
-    <image source="C:/Users/panda/OneDrive/Desktop/Senior Proj/UofM/Assets/Art/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <image source="../../Art/roguelikeSheet_transparent.png" width="968" height="526"/>
 </tileset>
